ed8a2586f04c888b17c7f798c74c833bde18be95
PRCSGH <78178853+PRCSGH@users.noreply.github.com>
2022-01-10 10:43:05 -0400

    Modulo Externo

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'application': True,
+    'depends': ['base',],
+    'data':[
+        'security/ir.model.access.csv', 
+        'views/estate_menus.xml',
+        ]
+}
